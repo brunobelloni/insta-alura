80c37f82c5b4fb5156e9ce7b9a775b9c39645f09
brunobelloni <bruno.belloni@hotmail.com>
2018-12-07 23:53:16 -0200

    view condicional

--- src/components/Post.js
+++ src/components/Post.js
@@ -8,14 +8,29 @@ export default class Post extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            foto: { ...this.props.foto, likers: [{}] }
+            foto: this.props.foto
         }
     }
 
     like() {
+        const { foto } = this.state;
+
+        let novaLista = []
+        if (!foto.likeada) {
+            novaLista = [
+                ...foto.likers,
+                { login: 'meuUsuario' }
+            ]
+        } else {
+            novaLista = foto.likers.filter(liker => {
+                return liker.login !== 'meuUsuario'
+            })
+        }
+
         const fotoAtualizada = {
-            ...this.state.foto,
-            likeada: !this.state.foto.likeada
+            ...foto,
+            likeada: !foto.likeada,
+            likers: novaLista
         }
 
         this.setState({ foto: fotoAtualizada })
